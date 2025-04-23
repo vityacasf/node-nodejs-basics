6568fcd169c617db282d15fb43b4370e7c865df2
vityacasf <neustupov01@mail.ru>
2025-04-23 13:38:25 +0300

    feat: add create.js code

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import { open } from 'node:fs';
+import { access } from 'node:fs/promises';
+
 const create = async () => {
-    // Write your code here 
+    try{
+    await access("./files/fresh.txt");
+    console.log("FS operation failed"); 
+    } catch{
+
+    }
+    open("./files/fresh.txt", "w", (err) => {
+        if(err) throw err();
+        console.log("file created");
+    })
+
 };
 
 await create();
